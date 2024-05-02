9c377fc3b242b0ca4f5e07b9f50b2ebdbd080446
tlee10333 <trinityl10333@gmail.com>
2024-05-02 10:06:10 -0400

    Cleaned main with working send and recv

--- auto_code/auto_client.c
+++ auto_code/auto_client.c
@@ -12,6 +12,7 @@
 #define MSG_SIZE 200
 #define MAX_CLIENTS 150
 #define MYPORT 7400
+#define HOSTNAME "127.0.0.1"
 
 void try_connect(int client_socket, struct sockaddr_in server_addr) {
   // Try connecting
@@ -27,35 +28,6 @@ FILE *get_socket_file(int client_socket) {
     error_and_exit("Couldn't open socket as file stream\n");
   }
   return socket_file;
-}
-
-int command_recv(FILE *socket_file) {
-  char *recv_line = NULL;
-  size_t recv_line_size = 0;
-  // Wait to receive a message from the server
-  if (getline(&recv_line, &recv_line_size, socket_file) == -1) {
-    printf("Error in receiving message\n");
-    return -1;
-  }
-
-  // Read the output of the command and send it back to the server
-  char output_buffer[4096]; // Adjust buffer size as needed
-
-  system(recv_line);
-
-  while (fgets(output_buffer, sizeof(output_buffer), stdout) != NULL) {
-    printf("%s\n", output_buffer);
-    if (fputs(output_buffer, socket_file) == EOF) {
-      printf("Error sending response to server\n");
-      free(recv_line);
-      return -1;
-    }
-  }
-  fflush(socket_file); // Ensure the data is sent immediately
-
-  free(recv_line);
-
-  return 0;
 }
 
 int test_recv_send(FILE *socket_file) {
@@ -87,49 +59,30 @@ int test_recv_send(FILE *socket_file) {
       return -1;
     }
   }
+  
   fflush(socket_file); // Ensure the data is sent immediately
-
-  // Close the output pipe
-  pclose(output_pipe);
-
-  // Clean up
-  free(recv_line);
+  pclose(output_pipe); // Close the output pipe
+  free(recv_line); // Clean up
 
   return 0;
 }
 int main(int argc, char *argv[]) {
-  int i = 0, client_sockfd;
-  struct sockaddr_in server_address;
-  int addresslen = sizeof(struct sockaddr_in), fd;
-  char fd_array[MAX_CLIENTS];
-  fd_set readfds, testfds, clientfds;
-  char msg[MSG_SIZE + 1];
-  char kb_msg[MSG_SIZE + 10];
-
   /*Client variables*/
-
   int sockfd;
-  int result;
-  char hostname[MSG_SIZE] = "127.0.0.1";
-  int port = 7400;
-
   struct hostent *hostinfo;
-  char alias[MSG_SIZE];
-  int clientid;
 
   /*Client*/
-
   printf("\n*** Client program starting (enter \"quit\" to stop): \n");
   fflush(stdout);
 
   /* Create a socket for the client */
   sockfd = open_tcp_socket();
   /* Name the socket, as agreed with the server */
-  hostinfo = gethostbyname(hostname); /* look for host's name */
+  hostinfo = gethostbyname(HOSTNAME); /* look for host's name */
   // Make a sockaddr_in for binding/connecting
   struct sockaddr_in address = {
       .sin_family = AF_INET,
-      .sin_port = (in_port_t)htons(port),
+      .sin_port = (in_port_t)htons(MYPORT),
       .sin_addr = *(struct in_addr *)*hostinfo->h_addr_list,
   };
 
