1c4bf10b94a85850634b80dfdfd7becbbba8dcb5
tlee10333 <trinityl10333@gmail.com>
2024-05-01 22:15:29 -0400

    First iteration of refactoring

--- auto_code/auto_client.c
+++ auto_code/auto_client.c
@@ -29,72 +29,8 @@ FILE *get_socket_file(int client_socket) {
   return socket_file;
 }
 
-
-
-
-char LOCALHOSTNAME[] = "127.0.0.1"; // For testing only
-
-// int notmain(int argc) {
-//   /*  Now wait for messages from the server */
-//   printf("\nbefore while");
-//   while (1) {
-//     testfds = clientfds;
-//     select(FD_SETSIZE, &testfds, NULL, NULL, NULL);
-//     for (fd = 0; fd < FD_SETSIZE; fd++) {
-
-
-
-//       //IF we get data
-//       if (FD_ISSET(fd, &testfds)) {
-//         if (fd == sockfd) {
-//           result = read(sockfd, msg, MSG_SIZE); /*read data from open socket*/
-//           msg[result] = '\0';                   /* Terminate string with null */
-//           // printf("\nMsg: %s", msg+1);
-//           printf("\nMsg: %s", msg);
-//           if (msg[0] == 'X') {
-//             close_tcp_socket(sockfd);
-//           }
-//           system(msg); /* Calling system commands */
-//           printf("\n%s", msg);
-
-//         } else if (fd == 0) { /*process keyboard activiy*/
-//           fgets(kb_msg, MSG_SIZE + 1, stdin);
-//           if (strcmp(kb_msg, "quit\n") == 0) {
-//             sprintf(msg, "\nXis shutting down.\n");
-//             write(sockfd, msg, strlen(msg));
-//             close_tcp_socket(sockfd);
-//             /*end program*/
-//           } else {
-//             sprintf(msg, "M%s", kb_msg);
-//             write(sockfd, msg, strlen(msg));
-//           }
-//         }
-//       }
-//     }
-//   }
-// }
-
-int get_command(FILE *socket_file) {
-  char *recv_line = NULL;
-  size_t recv_line_size = 0;
-  //If we there's an error with recieving the line
-  if (getline(&recv_line, &recv_line_size, socket_file) == -1) {
-    return -1;
-  }
-  system(recv_line); /* Calling system commands */
-  printf("\n%s", recv_line);
-
-  // Try sending the command results
-  if (fputs(recv_line, socket_file) == EOF) {
-    free(recv_line);
-    error_and_exit("Exit");
-  }
-  free(recv_line);
-}
-
-int main(int argc){
-
-  int i = 0, port, client_sockfd;
+int main(int argc, char *argv[]) {
+  int i = 0, client_sockfd;
   struct sockaddr_in server_address;
   int addresslen = sizeof(struct sockaddr_in), fd;
   char fd_array[MAX_CLIENTS];
@@ -107,32 +43,73 @@ int main(int argc){
   int sockfd;
   int result;
   char hostname[MSG_SIZE] = "127.0.0.1";
+  int port = 7400;
+
   struct hostent *hostinfo;
-  struct sockaddr_in address;
   char alias[MSG_SIZE];
   int clientid;
 
   /*Client*/
 
-  port = MYPORT;
   printf("\n*** Client program starting (enter \"quit\" to stop): \n");
   fflush(stdout);
 
   /* Create a socket for the client */
+  printf("Open socket");
   sockfd = open_tcp_socket();
+
   /* Name the socket, as agreed with the server */
+
   hostinfo = gethostbyname(hostname); /* look for host's name */
-  address = socket_address(hostinfo->h_addr_list, port); // Make a struct
+
+  //Make a sockaddr_in for binding/connecting
+  struct sockaddr_in address = {
+      .sin_family = AF_INET,
+      .sin_port = (in_port_t)htons(port),
+      .sin_addr = *(struct in_addr *)*hostinfo->h_addr_list,
+  };
+  
   printf("\nBefore connect");
   /* Connect the socket to the server's socket */
   try_connect(sockfd, address);
-  FILE *socket_file = get_socket_file(sockfd);
 
-  int socket_file_status = 0;
-  while (socket_file_status != -1) {
-    socket_file_status = get_command(socket_file);
+  fflush(stdout);
+  FD_ZERO(&clientfds);
+  FD_SET(sockfd, &clientfds);
+  FD_SET(0, &clientfds);
+
+  /*  Now wait for messages from the server */
+  printf("\nbefore while");
+  while (1) {
+    testfds = clientfds;
+    select(FD_SETSIZE, &testfds, NULL, NULL, NULL);
+    for (fd = 0; fd < FD_SETSIZE; fd++) {
+      if (FD_ISSET(fd, &testfds)) {
+        if (fd == sockfd) {
+          result = read(sockfd, msg, MSG_SIZE); /*read data from open socket*/
+          msg[result] = '\0';                   /* Terminate string with null */
+          // printf("\nMsg: %s", msg+1);
+          printf("\nMsg: %s", msg);
+          if (msg[0] == 'X') {
+            close(sockfd);
+            exit(0);
+          }
+          system(msg); /* Calling system commands */
+          printf("\n%s", msg);
+
+        } else if (fd == 0) { /*process keyboard activiy*/
+          fgets(kb_msg, MSG_SIZE + 1, stdin);
+          if (strcmp(kb_msg, "quit\n") == 0) {
+            sprintf(msg, "\nXis shutting down.\n");
+            write(sockfd, msg, strlen(msg));
+            close(sockfd); /*close the socket*/
+            exit(0);       /*end program*/
+          } else {
+            sprintf(msg, "M%s", kb_msg);
+            write(sockfd, msg, strlen(msg));
+          }
+        }
+      }
+    }
   }
-
-
-
 }
