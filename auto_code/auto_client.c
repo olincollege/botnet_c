66a41dd5a702e97cbe3fa7a62d980359aacfbd24
tlee10333 <trinityl10333@gmail.com>
2024-05-03 16:05:58 -0400

    Adding client with MAC Address

--- auto_code/auto_client.c
+++ auto_code/auto_client.c
@@ -1,13 +1,17 @@
+#include "util.h"
 #include <arpa/inet.h>
+#include <dirent.h>
+#include <linux/if.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>      // exit, EXIT_FAILURE
+#include <stdnoreturn.h> // noreturn
 #include <string.h>
+#include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-#include "util.h"
 #define MSG_SIZE 4096
 #define MYPORT 7400
 #define HOSTNAME "127.0.0.1"
@@ -33,6 +37,56 @@ FILE *recv_exec_msg(int sockfd, char msg[], int msg_size) {
   }
 }
 
+char *find_devices(char path[]) {
+  struct dirent *de; // Pointer for directory entry
+  char network_interface[20];
+
+  // opendir() returns a pointer of DIR type.
+  DIR *dr = opendir(path);
+
+  // Error Checking
+  if (dr == NULL) {
+    error_and_exit("Could not open given directory");
+  }
+
+  // Hard_coded to look for the WLAN (wl)
+  while ((de = readdir(dr)) != NULL) {
+    if (de->d_name[0] == 'w' && de->d_name[1] == 'l') {
+      closedir(dr);
+      return strcpy(network_interface, de->d_name);
+    }
+  }
+
+  closedir(dr);
+  return NULL;
+}
+
+int mac_address(char interface[], char addr[]) {
+  struct ifreq s;
+  int fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+  char temp[3];
+  // Error Checking
+  if (interface == NULL) {
+    error_and_exit("No Valid interface\n");
+  }
+  if (fd == -1) {
+    error_and_exit("Can't open socket\n");
+  }
+  strcpy(s.ifr_name, interface);
+  if (0 == ioctl(fd, SIOCGIFHWADDR, &s)) {
+    addr[0] = '\0'; // Initialize addr as an empty string
+    for (int i = 0; i < 6; ++i) {
+      sprintf(temp, "%02X", (unsigned char)s.ifr_hwaddr.sa_data[i]);
+      strcat(addr, temp);
+    }
+    close(fd); // Close the socket
+    return 0;
+  }
+
+  close(fd); // Close the socket
+  return -1; // Didn't work.
+}
+
 int main() {
   /* Create a socket for the client */
   int sockfd = open_tcp_socket();
@@ -55,6 +109,18 @@ int main() {
   char msg[MSG_SIZE];
   char output_buffer[MSG_SIZE]; // Adjust buffer size as needed
   char result_buffer[MSG_SIZE]; // Initialize an empty string buffer
+
+  // Send MAC Address Once
+  char *wl_interface = find_devices("/sys/class/net");
+  char mac_addr[12];
+  if (mac_address(wl_interface, mac_addr) == -1) {
+    printf("Failed to get MAC Address");
+  }
+
+  printf("SENDING MAC ADDRESS: %s \n ", mac_addr);
+  if (send(sockfd, mac_addr, strlen(mac_addr), 0) == -1) {
+    error_and_exit("Error sending response to server\n");
+  }
 
   while (1) {
     // Process the received message here (e.g., execute commands)
