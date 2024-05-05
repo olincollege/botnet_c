eaf1006cd42c0770418bc4f4244f9f8cf848c446
tlee10333 <trinityl10333@gmail.com>
2024-05-05 17:06:08 -0400

    Decreasing some warnings from clang and tidy

--- client/client_functions.c
+++ client/client_functions.c
@@ -15,9 +15,9 @@
 
 #include "../util/util.h"
 
-FILE* recv_exec_msg(int sockfd, char msg[], int msg_size) {
+FILE* recv_exec_msg(int sockfd, char msg[], size_t msg_size) {
   /* Receive and process messages from the server */
-  int bytes_received = recv(sockfd, msg, msg_size, 0);
+  ssize_t bytes_received = recv(sockfd, msg, msg_size, 0);
 
   if (bytes_received < 0) {
     error_and_exit("Error reading from socket \n");
@@ -37,51 +37,56 @@ FILE* recv_exec_msg(int sockfd, char msg[], int msg_size) {
 }
 
 char* find_devices(char path[]) {
-  struct dirent* de;  // Pointer for directory entry
   char network_interface[20];
 
   // opendir() returns a pointer of DIR type.
-  DIR* dr = opendir(path);
+  DIR* dr_pointer = opendir(path);
 
   // Error Checking
-  if (dr == NULL) {
+  if (dr_pointer == NULL) {
     error_and_exit("Could not open given directory \n");
   }
 
   // Hard_coded to look for the WLAN (wl)
-  while ((de = readdir(dr)) != NULL) {
-    if (de->d_name[0] == 'w' && de->d_name[1] == 'l') {
-      closedir(dr);
-      return strcpy(network_interface, de->d_name);
+  struct dirent* dr_entry = readdir(dr_pointer);  // Pointer for directory entry
+  while ((dr_entry = readdir(dr_pointer)) != NULL) {
+    if (dr_entry->d_name[0] == 'w' && dr_entry->d_name[1] == 'l') {
+      closedir(dr_pointer);
+      return strcpy(network_interface, dr_entry->d_name);
     }
   }
 
-  closedir(dr);
+  closedir(dr_pointer);
   return NULL;
 }
 
 int mac_address(char interface[], char addr[]) {
-  struct ifreq s;
-  int fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+  struct ifreq sock;
+  int fd_socket = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
   char temp[3];
+  const int addr_size = 6;
   // Error Checking
   if (interface == NULL) {
     error_and_exit("No Valid interface\n");
   }
-  if (fd == -1) {
+  if (fd_socket == -1) {
     error_and_exit("Can't open socket\n");
   }
-  strcpy(s.ifr_name, interface);
-  if (0 == ioctl(fd, SIOCGIFHWADDR, &s)) {
+  strcpy(sock.ifr_name, interface);
+  if (0 == ioctl(fd_socket, SIOCGIFHWADDR, &sock)) {
     addr[0] = '\0';  // Initialize addr as an empty string
-    for (int i = 0; i < 6; ++i) {
-      sprintf(temp, "%02X", (unsigned char)s.ifr_hwaddr.sa_data[i]);
+    for (int count = 0; count < addr_size; ++count) {
+      if (sprintf(temp, "%02X", (unsigned char)sock.ifr_hwaddr.sa_data[count]) <
+          0) {
+        error_and_exit("Getting MAC Address \n");
+      }
+
       strcat(addr, temp);
     }
-    close(fd);  // Close the socket
+    close(fd_socket);  // Close the socket
     return 0;
   }
 
-  close(fd);  // Close the socket
-  return -1;  // Didn't work.
+  close(fd_socket);  // Close the socket
+  return -1;         // Didn't work.
 }
